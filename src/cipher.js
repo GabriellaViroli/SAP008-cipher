83a793a57b8a39c2822e3359286db031a51e0eb0
GabriellaViroli <gvccgabriella@gmail.com>
2022-07-18 20:04:31 -0300

    adiciona parametro offset, estiliza botoes

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,14 @@
-const cipher = {
-  // ...
-};
+function cipher(texto, offset) {
+    let letraCifrada;
+    let charCodigo;
+    let resultado = "";
 
+    for(let posLetra = 0; posLetra < texto.length; posLetra++) {
+        charCodigo = (texto[posLetra].charCodeAt(0) - 65 + offset) % 26 + 65
+        letraCifrada = String.fromCharCode(charCodigo)
+        resultado = resultado + letraCifrada
+    }
+
+    return resultado
+}
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+let btnCifre = document.getElementById("btnCifre")
+
+btnCifre.onclick = function() {
+    let offset = parseInt(document.getElementById("casas").value)
+    let valorDigitado = criptografia.value.toUpperCase()
+    let valorCifrado = cipher(valorDigitado, offset)
+    console.log(valorCifrado)
+
+}
