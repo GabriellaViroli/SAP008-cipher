5d47fdf89e14228bf2df2dae44875d44238d427d
GabriellaViroli <gvccgabriella@gmail.com>
2022-07-25 12:34:52 -0300

    cria funçao decode, normaliza com documentaçao para testes, estiliza o site

--- src/cipher.js
+++ src/cipher.js
@@ -1,14 +1,44 @@
-function cipher(texto, offset) {
+function encode(offset, texto) {
+    // se o tipo de offset não for do tipo Int lançar TypeError
+    // se o tipo de texto não for do tipo String lançar TypeError
+    if (typeof offset != "number") {
+        throw TypeError()
+    }
+    if (typeof texto != "string") {
+        throw TypeError()
+    }
+
     let letraCifrada;
     let charCodigo;
     let resultado = "";
 
     for(let posLetra = 0; posLetra < texto.length; posLetra++) {
-        charCodigo = (texto[posLetra].charCodeAt(0) - 65 + offset) % 26 + 65
+        charCodigo = (texto.charCodeAt(posLetra) - 65 + offset) % 26 + 65
         letraCifrada = String.fromCharCode(charCodigo)
         resultado = resultado + letraCifrada
     }
 
     return resultado
 }
+
+function decode(offset, texto) {
+    if (typeof offset != "number") {
+        throw TypeError()
+    }
+    if (typeof texto != "string") {
+        throw TypeError()
+    }
+    let letraCifrada;
+    let charCodigo;
+    let resultado = "";
+
+    for(let posLetra = 0; posLetra < texto.length; posLetra++) {
+        charCodigo = (texto.charCodeAt(posLetra) - 90 - offset) % 26 + 90
+        letraCifrada = String.fromCharCode(charCodigo)
+        resultado = resultado + letraCifrada
+    }
+    return resultado
+}
+
+const cipher = {encode, decode}
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import cipher from './cipher.js';
 
 let btnCifre = document.getElementById("btnCifre")
+let btnDecifre = document.getElementById("btnDecifre")
+let inputCriptografia = document.getElementById("criptografia")
+let inputDescriptografia = document.getElementById("descriptografia")
+let inputOffset = document.getElementById("casas")
 
-btnCifre.onclick = function() {
-    let offset = parseInt(document.getElementById("casas").value)
-    let valorDigitado = criptografia.value.toUpperCase()
-    let valorCifrado = cipher(valorDigitado, offset)
-    console.log(valorCifrado)
-
+btnCifre.onclick = function encode () {
+    let offset = parseInt(inputOffset.value)
+    let valorDigitado = inputCriptografia.value.toUpperCase()
+    let valorCifrado = cipher.encode(offset, valorDigitado)
+    inputDescriptografia.value = valorCifrado
 }
+
+btnDecifre.onclick = function decode () {
+    let offset = parseInt(inputOffset.value)
+    let valorDigitado = inputDescriptografia.value.toUpperCase()
+    let valorDecifrado = cipher.decode(offset, valorDigitado)
+    inputCriptografia.value = valorDecifrado
+
+} 
